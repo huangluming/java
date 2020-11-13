ed1475dd6e3a42d46a24c2ffe4852f04488e9ca2
huangluming <985285927@qq.com>
2020-11-13 12:20:52 +0800

    class major

---
+++ src/com/imooc/subject/Major.java
@@ -0,0 +1,45 @@
+package com.imooc.subject;
+
+public class Major {
+	private String majorName;
+	private String majorCode;
+	private int majorYear;
+	
+	public Major() {
+		super();
+	}
+	public Major(String majorName, String majorCode, int majorYear) {
+		super();
+		this.setMajorName(majorName);
+		this.setMajorYear(majorYear);
+		this.setMajorCode(majorCode);
+	}
+	public String getMajorName() {
+		return majorName;
+	}
+	public void setMajorName(String majorName) {
+		this.majorName = majorName;
+	}
+	public String getMajorCode() {
+		return majorCode;
+	}
+	public void setMajorCode(String majorCode) {
+		this.majorCode = majorCode;
+	}
+	public int getMajorYear() {
+		return majorYear;
+	}
+	public void setMajorYear(int majorYear) {
+		if(majorYear<=0) {
+			return;
+		}
+		this.majorYear = majorYear;
+	}
+	@Override
+	public String toString() {
+		return "Major [majorName=" + majorName + ", majorCode=" + majorCode + ", majorYear=" + majorYear + "]";
+	}
+	
+	
+
+}
